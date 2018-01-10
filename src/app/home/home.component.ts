c624427557e5ee5f7e1a39ab4f8d917ac26317a3
clement <clement.deboos@gmail.com>
2018-01-10 10:24:12 +0100

    Cookies + correct

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -7,9 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  cookies = 0;
+  clickerPrice = 10;
+  clickers = 0;
+
   constructor() { }
 
   ngOnInit() {
+    window.setInterval(this.autoincrement.bind(this), 1000);
   }
 
+  addCookies() {
+    this.cookies += 1;
+    return this.cookies;
+  }
+
+  addClickers() {
+    if (this.clickerPrice <= this.cookies) {
+      this.cookies -= this.clickerPrice;
+      this.clickerPrice = Math.ceil(this.clickerPrice *= 1.07);
+      this.clickers += 1;
+    }
+  }
+
+  autoincrement() {
+    this.cookies += this.clickers;
+  }
 }
